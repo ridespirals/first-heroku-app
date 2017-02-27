7535de0d992034013a62bdc3076e9f19eaeb453f
John Varga <john.varga@unifilabs.com>
2017-02-27 14:29:30 -0800

    fix server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/'))
+app.use(express.static(`${__dirname}/build/`))
 
-app.get('/', (req, res) =>  {
-    res.send('Hello World! Get over it!')
+app.get('*', (req, res) =>  {
+    res.sendFile(`${__dirname}/build/index.html`)
 })
 
 app.listen(app.get('port'), () => {
