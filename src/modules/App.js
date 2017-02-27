db5f83ab2d947fffc17e74fbde4671f8bff5a7c2
John Varga <john.varga@unifilabs.com>
2017-02-27 09:35:46 -0800

    add a log for testing

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default React.createClass({
+    componentDidMount() {
+        console.log(`I'm a react component and I mounted!`);
+    },
     render() {
         return <div>Hello from React!</div>
     }
