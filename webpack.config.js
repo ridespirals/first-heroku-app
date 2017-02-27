2672844f60b6137f0a6d6dc0593faf40e6b26c34
John Varga <john.varga@unifilabs.com>
2017-02-26 18:11:59 -0800

    set up more deps, webpack config, prep for react

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: [ './index.js' ],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: 'node_modules',
+            loader: 'babel-loader',
+            query: { presets: [ 'es2015', 'react' ] }
+        }]
+    },
+    resolve: {
+        extensions: [ '', '.js', '.jsx']
+    }
+}
