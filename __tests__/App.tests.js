9eeab6fb8956a70a2f6c9eeb9a367f7c5819421f
John Varga <john.varga@unifilabs.com>
2017-02-26 22:51:10 -0800

    test react component

--- __tests__/App.tests.js
+++ __tests__/App.tests.js
@@ -1,10 +1,22 @@
-const chai = require('chai')
+import chai from 'chai'
 const expect = chai.expect
+import React from 'react'
+import renderer from 'react-test-renderer'
+import App from '../src/modules/App'
 
-describe('App', () => {
+test('App', () => {
 
     it('should exist', () => {
         const App = require('../src/modules/App.js')
         expect(App).to.be.defined
     })
+
+    it('should render a div', () => {
+        const component = renderer.create(
+            <App />
+        )
+        let tree = component.toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+
 })
